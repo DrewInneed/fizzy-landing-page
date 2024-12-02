e9c91deccd0f19354bc244c69a3463c040a85d07
Andrew Le Nguyen <drew.contact.2024@gmail.com>
2024-12-02 18:06:56 +0700

    rebuild: to turn off dev bar

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -3,7 +3,6 @@
 import dynamic from "next/dynamic";
 
 export const components = {
-  alternating_text: dynamic(() => import("./AlternatingText")),
   big_text: dynamic(() => import("./BigText")),
   carousel: dynamic(() => import("./Carousel")),
   hero: dynamic(() => import("./Hero")),
